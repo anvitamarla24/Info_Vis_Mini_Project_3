ec2fb17fda8ac9e29f3d67af3cb975d1852ff3ee
anvitamarla24 <56138555+anvitamarla24@users.noreply.github.com>
2020-05-13 15:06:14 -0400

    Add files via upload

--- visualizations/vis1.js
+++ visualizations/vis1.js
@@ -1,8 +1,9 @@
 function vis1(data, div) {
-  const margin = {top: 40, right: 20, bottom: 40, left: 100};
+	
+  const margin = {top: 30, right: 100, bottom: 50, left: 100};
+  const visWidth = 600 - margin.left - margin.right;
+  const visHeight = 680 - margin.top - margin.bottom;
 
-  const visWidth = 700 - margin.left - margin.right;
-  const visHeight = 400 - margin.top - margin.bottom;
 
   const svg = div.append('svg')
       .attr('width', visWidth + margin.left + margin.right)
@@ -11,60 +12,209 @@ function vis1(data, div) {
   const g = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+  const data2 = data.map(currentElement => ({
+  'donor': currentElement.donor,
+  'recipient': currentElement.recipient,
+  'amount': currentElement.commitment_amount_usd_constant
+  // 'year': currentElement.year
+}));
+  
+  const data3 = data2.filter(d => d.amount > 0)
+  
+ function func_data_top10_receivers(data){ 
+  
+  const data_ = data.filter(d => d.commitment_amount_usd_constant > 0)
+  const cleanedData = data_.map(currentElement => ({
+  'country': currentElement.recipient,
+  'received': currentElement.commitment_amount_usd_constant
+  //'year': currentElement.year
+}));
+
+  const numPurpose = d3.rollup(cleanedData, v => d3.sum(v, d => d.received), d => d.country);
+  
+  const purposeCount = Array.from(numPurpose, ([country, total]) => ({country, total}));
+
+    return purposeCount.sort((a, b) => d3.descending(a.total, b.total)).slice(0, 10) 
+
+}
+  const data_top10_receivers = func_data_top10_receivers(data);
+  const top10_receivers = data_top10_receivers.map(d => d.country);
+  
+ function func_data_top20_donors(data){  
+  const data_ = data.filter(d => d.commitment_amount_usd_constant > 0)
+  const cleanedData = data_.map(currentElement => ({
+  'country': currentElement.donor,
+  'donated': currentElement.commitment_amount_usd_constant,
+  'year': currentElement.year
+}));
+
+  const numPurpose = d3.rollup(cleanedData, v => d3.sum(v, d => d.donated), d => d.country);
+  
+  const purposeCount = Array.from(numPurpose, ([country, total]) => ({country, total}));
+
+    return purposeCount.sort((a, b) => d3.descending(a.total, b.total)).slice(0, 20) 
+
+}
+  
+  const data_top20_donors = func_data_top20_donors(data);
+  const top20_donors = data_top20_donors.map(d => d.country);
+  
+  const filteredData = data3.filter(d => top10_receivers.includes(d.recipient));
+  const filteredData2 = filteredData.filter(d => top20_donors.includes(d.donor));
+  
+  function func_numRequestsByAgencyAndBorough(filteredData2){
+  const agencyToBoroughToCount = d3.rollup(filteredData2.filter(d => d.amount > 0),
+                                     v => d3.sum(v, d => d.amount),
+                                     d => d.donor, d => d.recipient);
+  
+  return Array.from(agencyToBoroughToCount, ([donor, boroughToCount]) => ({
+    donor: donor,
+    recipients: Array.from(boroughToCount, ([recipient, amount]) => ({recipient, amount}))
+  }));
+}
+  
+  const numRequestsByAgencyAndBorough = func_numRequestsByAgencyAndBorough(filteredData2);
+  
+    const cScale = d3.scalePow()
+                    .exponent(0.5)
+                    .domain([d3.min(numRequestsByAgencyAndBorough, d => d3.min(d.recipients, b => b.amount)), d3.max(numRequestsByAgencyAndBorough, d => d3.max(d.recipients, b => b.amount))])
+                    .range(["#F1B8FA","#56249F"])
+					//.range(["#73b2d7", "#08306b"])
+                    .interpolate(d3.interpolateHcl)
+  
+  // const cScale = d3.scaleSequentialSqrt([0, d3.max(numRequestsByAgencyAndBorough, d => d3.max(d.recipients, b => b.amount))], d3.interpolatePuRd)
+  
+  
   // add title
 
   g.append("text")
     .attr("x", visWidth / 2)
-    .attr("y", -margin.top + 5)
+    .attr("y", -margin.top)
     .attr("text-anchor", "middle")
     .attr("dominant-baseline", "hanging")
     .attr("font-family", "sans-serif")
     .attr("font-size", "16px")
-    .text("Game Scores");
-
+    .text("Relationship b/w Countries");
+  
   // create scales
 
-  const x = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.score)]).nice()
-    .range([0, visWidth]);
+  const x = d3.scalePoint()
+      .domain(top10_receivers)
+      .range([0, visWidth])
+      .padding(0.5);
+  
+  const y = d3.scalePoint()
+      .domain(top20_donors)
+      .range([0, visHeight])
+      .padding(0.5);
+  
+  const maxRadius = 10;
+  const radius = d3.scaleSqrt()
+      .domain([0, d3.max(numRequestsByAgencyAndBorough, d => d3.max(d.recipients, b => b.amount))])
+      .range([2, maxRadius]);
+	  
+	  
+  top10_receivers.forEach(country => {
+            g.append("line")
+                .style("stroke", "#ebebe0")
+                .style("stroke-dasharray", ("3, 3"))
+                .attr("y1", visHeight)
+                .attr("y2", 0)
+                .attr("x1", x(country))
+                .attr("x2", x(country))
+        })
+  
+  top20_donors.forEach(country => {
+            g.append("line")
+                .style("stroke", "#ebebe0")
+                .style("stroke-dasharray", ("3,3"))
+                .attr("y1", y(country))
+                .attr("y2", y(country))
+                .attr("x1", visWidth)
+                .attr("x2", 0)
+        })
+  
+  // add legend
+  
+  const legend = g.append("g")
+      .attr("transform", `translate(${visWidth + margin.right - 70}, 0)`)
+    .selectAll("g")
+    .data([1568, 12207518000, 24415034432, 36622550863, 48830067295])
+    .join("g")
+      .attr("transform", (d, i) => `translate(0, ${i * 2.5 * maxRadius})`);
+  
+  legend.append("circle")
+    .attr("r", d => radius(d))
+    .attr("fill", d => cScale(d));
 
-  const sortedNames = data.sort((a, b) => d3.descending(a.score, b.score))
-      .map(d => d.name);
-
-  const y = d3.scaleBand()
-    .domain(sortedNames)
-    .range([0, visHeight])
-    .padding(0.2);
-
+  legend.append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 12)
+    .attr("dominant-baseline", "middle")
+    .attr("x", maxRadius + 5)
+    .text((d) => {
+                    let million = 1000000
+                    let billion = 1000000000
+                    if(d < million){
+                        return "$" + Math.ceil(d/1000) + " K"
+                    } if(d < billion){
+                        return "$" + Math.ceil(d/million) + " M"
+                    }
+                    return "$" + Math.ceil(d/billion) + " B"
+                })
+    // .text(d => `${Math.round(d*0.000001)}M`);
+  
   // create and add axes
-
-  const xAxis = d3.axisBottom(x);
-
+  
+  const xAxis = d3.axisBottom(x).tickFormat(d => {
+  if (d === 'South Africa')
+    return 'S.Africa'
+  else if(d === 'Saudi Arabia')
+    return 'S.Arabia'
+  return d
+  });
+  
   g.append("g")
-    .attr("transform", `translate(0, ${visHeight})`)
-    .call(xAxis)
-    .call(g => g.selectAll(".domain").remove())
+      .attr("transform", `translate(0, ${visHeight})`)
+      .call(xAxis)
+      .call(g => g.selectAll(".domain").remove())
     .append("text")
-    .attr("x", visWidth / 2)
-    .attr("y", 40)
-    .attr("fill", "black")
-    .attr("text-anchor", "middle")
-    .text("Score");
-
+      .attr("x", visWidth / 2)
+      .attr("y", 40)
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .text("Recipients");
+  
   const yAxis = d3.axisLeft(y);
-
+  
   g.append("g")
-    .call(yAxis)
-    .call(g => g.selectAll(".domain").remove());
-
-  // draw bars
-
-  g.selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("x", d => 0)
-    .attr("y", d => y(d.name))
-    .attr("width", d => x(d.score))
-    .attr("height", d => y.bandwidth())
-    .attr("fill", "steelblue");
+      .call(yAxis)
+      .call(g => g.selectAll(".domain").remove())
+    .append("text")
+      .attr("x", -65)
+      .attr("y", visHeight / 2)
+      .attr("fill", "black")
+      .attr("dominant-baseline", "middle")
+      .text("Donors");
+  
+  // draw points
+  
+  const rows = g.selectAll(".row")
+    .data(numRequestsByAgencyAndBorough)
+    .join("g")
+      .attr("transform", d => `translate(0, ${y(d.donor)})`);
+  
+  rows.selectAll("circle")
+    .data(d => d.recipients)
+    .join("circle")
+      .attr("cx", d => x(d.recipient))
+      .attr("cy", d => 0)
+      .attr("fill", d => cScale(d.amount))
+      .attr("r", d => radius(d.amount));
+  
+   rows.selectAll("circle")
+     .data(d => d.recipients)
+     .join("circle")
+     .append("title")
+      .text(d => `Amount Donated: ${Math.round(d.amount*0.000001)}Million`)
 }
--- visualizations/vis2.js
+++ visualizations/vis2.js
@@ -1,34 +1,276 @@
-function vis2(geoJSON, div) {
-  const margin = {top: 20, right: 20, bottom: 20, left: 20};
+function vis2(data, mini3_vis2_data, div) {
+  const margin = {top: 30, right: 100, bottom: 50, left: 100};
+  const visWidth = 600 - margin.left - margin.right;
+  const visHeight = 680 - margin.top - margin.bottom;
 
-  const visWidth = 700 - margin.left - margin.right;
-  const visHeight = 400 - margin.top - margin.bottom;
 
   const svg = div.append('svg')
-      .attr('width', visWidth + margin.left + margin.right)
+      .attr('width', screen.width)
       .attr('height', visHeight + margin.top + margin.bottom);
+	  //visWidth + margin.left + margin.right
 
-  const g = svg.append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-  const projection =  d3.geoEqualEarth()
-      .fitSize([visWidth, visHeight], geoJSON);
-
-  const path = d3.geoPath().projection(projection);
-
-  g.selectAll('.border')
-    .data(geoJSON.features)
-    .join('path')
-      .attr('class', 'border')
-      .attr('d', path)
-      .attr('fill', '#dcdcdc')
-      .attr('stroke', 'white');
+  const g = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+	
+  const top_5_purposes = ["Air transport",
+                        "Rail transport",
+                        "Industrial development",
+                        "Power generation/non-renewable sources",
+                        "RESCHEDULING AND REFINANCING"];
+	
+const data2 = data.map(currentElement => ({
+  'donor': currentElement.donor,
+  'recipient': currentElement.recipient,
+  'amount': currentElement.commitment_amount_usd_constant
+  // 'year': currentElement.year
+}));
   
-  const mapOutline = d3.geoGraticule().outline();
+  const data3 = data2.filter(d => d.amount > 0)
   
-  g.append('path')
-    .datum(mapOutline)
-    .attr('d', path)
-    .attr('stroke', '#dcdcdc')
-    .attr('fill', 'none');
+ function func_data_top10_receivers(data){ 
+  
+  const data_ = data.filter(d => d.commitment_amount_usd_constant > 0)
+  const cleanedData = data_.map(currentElement => ({
+  'country': currentElement.recipient,
+  'received': currentElement.commitment_amount_usd_constant
+  //'year': currentElement.year
+}));
+
+  const numPurpose = d3.rollup(cleanedData, v => d3.sum(v, d => d.received), d => d.country);
+  
+  const purposeCount = Array.from(numPurpose, ([country, total]) => ({country, total}));
+
+    return purposeCount.sort((a, b) => d3.descending(a.total, b.total)).slice(0, 10) 
+
+}
+  const data_top10_receivers = func_data_top10_receivers(data);
+  const top10_receivers = data_top10_receivers.map(d => d.country);
+  
+ function func_data_top20_donors(data){  
+  const data_ = data.filter(d => d.commitment_amount_usd_constant > 0)
+  const cleanedData = data_.map(currentElement => ({
+  'country': currentElement.donor,
+  'donated': currentElement.commitment_amount_usd_constant,
+  'year': currentElement.year
+}));
+
+  const numPurpose = d3.rollup(cleanedData, v => d3.sum(v, d => d.donated), d => d.country);
+  
+  const purposeCount = Array.from(numPurpose, ([country, total]) => ({country, total}));
+
+    return purposeCount.sort((a, b) => d3.descending(a.total, b.total)).slice(0, 20) 
+
+}
+  
+  const data_top20_donors = func_data_top20_donors(data);
+  const top20_donors = data_top20_donors.map(d => d.country);
+
+ // add title
+
+  g.append("text")
+    .attr("x", visWidth / 2)
+    .attr("y", -margin.top)
+    .attr("text-anchor", "middle")
+    .attr("dominant-baseline", "hanging")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "16px")
+    .text("Distribution of Purposes");
+  
+  // create scales
+
+  const x = d3.scalePoint()
+      .domain(top10_receivers)
+      .range([0, visWidth])
+      .padding(0.5);
+  
+  const y = d3.scalePoint()
+      .domain(top20_donors)
+      .range([0, visHeight])
+      .padding(0.5);
+  
+  const maxRadius = 15;
+  const radius = d3.scaleSqrt()
+      .domain([0, d3.max(mini3_vis2_data, d => d.total)])
+      .range([5, maxRadius]);
+	  
+	  
+  top10_receivers.forEach(country => {
+            g.append("line")
+                .style("stroke", "#ebebe0")
+                .style("stroke-dasharray", ("3, 3"))
+                .attr("y1", visHeight)
+                .attr("y2", 0)
+                .attr("x1", x(country))
+                .attr("x2", x(country))
+        })
+  
+  top20_donors.forEach(country => {
+            g.append("line")
+                .style("stroke", "#ebebe0")
+                .style("stroke-dasharray", ("3,3"))
+                .attr("y1", y(country))
+                .attr("y2", y(country))
+                .attr("x1", visWidth)
+                .attr("x2", 0)
+        })
+  
+  // create and add axes
+  
+  const xAxis = d3.axisBottom(x).tickFormat(d => {
+  if (d === 'South Africa')
+    return 'S.Africa'
+  else if(d === 'Saudi Arabia')
+    return 'S.Arabia'
+  return d
+  });
+  
+  g.append("g")
+      .attr("transform", `translate(0, ${visHeight})`)
+      .call(xAxis)
+      .call(g => g.selectAll(".domain").remove())
+    .append("text")
+      .attr("x", visWidth / 2)
+      .attr("y", 40)
+      .attr("fill", "black")
+      .attr("text-anchor", "middle")
+      .text("Recipients");
+  
+  const yAxis = d3.axisLeft(y);
+  
+  g.append("g")
+      .call(yAxis)
+      .call(g => g.selectAll(".domain").remove())
+    .append("text")
+      .attr("x", -65)
+      .attr("y", visHeight / 2)
+      .attr("fill", "black")
+      .attr("dominant-baseline", "middle")
+      .text("Donors");
+  
+  // draw points
+  
+ /* var arc = d3.arc()
+			.innerRadius(0)
+      .outerRadius(10)
+			// .outerRadius(d => radius(d.total))
+			
+  var pie = d3.pie()
+			.sort(null)
+			.value(d => d);*/
+  
+  const rows = g.selectAll(".row")
+    .data(mini3_vis2_data)
+    .join("g")
+      .attr("transform", d => `translate(${x(d.recipient)}, ${y(d.donor)})`)
+    .append("g").attr("class","pies")
+
+  var color = d3.schemeCategory10;
+  
+  var pies = rows.selectAll(".pies")
+		//.data(function(d) {console.log(d.data2); return pie(d.data2.split(['-'])); }) // I'm unsure why I need the leading 0.
+    .data((d) => {
+                let a = d3.pie().value((x) => x)([d.Air_transport, d.Rail_transport, d.Industrial_development, d.Power_generation_non_renewable_sources, d.RESCHEDULING_AND_REFINANCING]);
+                a.forEach((x, i) => {
+                    if(i == 0) x["key"] = "Air_transport";
+                    else if(i == 1) x["key"] = "Rail_transport";
+                    else if(i == 2) x["key"] = "Industrial_development";
+                    else if(i == 3) x["key"] = "Power_generation_non_renewable_sources";
+                    else x["key"] = "RESCHEDULING_AND_REFINANCING";
+                    x["total"] = d.total;
+                })
+                return a;
+            })
+		.enter()
+		.append('g')
+		.attr('class','arc');
+  
+ rows.append("title")
+      .text(d => `Amount of top 5 purposes donated by ${d.donor} to ${d.recipient}:
+Air transport: ${d.Air_transport}
+Rail transport: ${d.Rail_transport}
+Industrial development: ${d.Industrial_development}
+Power generation/non-renewable sources: ${d.Power_generation_non_renewable_sources}
+RESCHEDULING AND REFINANCING: ${d.RESCHEDULING_AND_REFINANCING}
+Total: ${d.total}`)
+  
+   pies.append("path")
+   .attr('d',(d,i) => {
+                 return d3.arc().innerRadius(0).outerRadius(radius(d.total))(d);
+             })
+     .attr("fill",function(d,i){
+            return color[i];      
+       })
+	
+	/*pies.append("path")
+	  .attr('d',arc)
+	.attr("fill",function(d,i){
+	return color[i];      
+	})*/
+	
+const color1 = d3.scaleOrdinal()
+      .domain(top_5_purposes)
+      .range(d3.schemeCategory10);
+  
+ function getCategoricalColorLegend(color1) {
+   
+  const size = 10;
+  
+  const legend = d3.create("svg:g");
+  
+  const rows = legend
+    .selectAll("g")
+    .data(color1.domain())
+    .join("g")
+      .attr("transform", (d, i) => `translate(0, ${i * size * 1.5})`);
+  
+  rows.append("rect")
+      .attr("height", size)
+      .attr("width", size)
+      .attr("fill", d => color1(d));
+  
+  rows.append("text")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("dominant-baseline", "hanging")
+      .attr("x", size * 1.5)
+      .text(d => d);
+  
+  return legend.node();
+}
+  
+ g.append(() => getCategoricalColorLegend(color1))
+      .attr("transform", `translate(${visWidth + 10}, 0)`)
+	  
+ const legend1 = g.append("g")
+      .attr("transform", `translate(${visWidth + margin.right - 70}, 100)`)
+    .selectAll("g")
+    .data([7238, 4536843991, 9073680744, 13610517497, 18147354250])
+    .join("g")
+      .attr("transform", (d, i) => `translate(0, ${i * 2.5 * maxRadius})`);
+  
+  legend1.append("circle")
+    .attr("r", d => radius(d))
+    .attr("fill", "none")
+    .attr("stroke", "#ccc")
+    // .attr("fill", "#dcdcdc");
+    .style("fill", "#d9d9d9")
+    .style("opacity", 0.98)
+
+  legend1.append("text")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 10)
+    .attr("dominant-baseline", "middle")
+    .attr("x", maxRadius + 5)
+    //.attr("y", maxRadius + 10)
+    .text((d) => {
+                    let million = 1000000
+                    let billion = 1000000000
+                    if(d < million){
+                        return "$" + Math.ceil(d/1000) + " K"
+                    } if(d < billion){
+                        return "$" + Math.ceil(d/million) + " M"
+                    }
+                    return "$" + Math.ceil(d/billion) + " B"
+                })
+	
 }
