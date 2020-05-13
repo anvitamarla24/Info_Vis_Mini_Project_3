abd42ff61a2f1b21ad9791dcbd1781b647065b77
anvitamarla24 <56138555+anvitamarla24@users.noreply.github.com>
2020-05-13 15:01:19 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Load the datasets and call the functions to make the visualizations
 
 Promise.all([
-  d3.csv('data/scores.csv', d3.autoType),
-  d3.json('data/countries.json'),
-]).then(([data, geoJSON]) => {
+  d3.csv('data/aiddata-countries-only - aiddata-countries-only.csv', d3.autoType),
+  d3.csv('data/mini3_vis2_data.csv', d3.autoType),
+]).then(([data, mini3_vis2_data]) => {
   vis1(data, d3.select('#vis1'));
-  vis2(geoJSON, d3.select('#vis2'));
+  vis2(data, mini3_vis2_data, d3.select('#vis2'));
 });
